69bf42d7f21cd0d6d3b789ea5bca7e3f47d8fc61
Louis Chan <louischan@oursky.com>
2018-09-28 15:17:12 +0800

    Fix index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,58 +1,56 @@
-declare module "react-native-oursky" {
-  import * as React from "react";
-  import { TextInputProps, TextInput } from "react-native";
+import * as React from "react";
+import { TextInputProps, TextInput } from "react-native";
 
-  export function createControlGroup(): {
-    ControlRoot: React.ComponentClass<{
-      children: React.ReactNode;
-    }>;
-    Control: React.ComponentClass<{
-      tabIndex: number;
-      children: (
-        props: {
-          ref: React.RefObject<TextInput>;
-          onSubmitEditing: TextInputProps["onSubmitEditing"];
-          blurOnSubmit: false;
-        }
-      ) => React.ReactNode;
-    }>;
+export function createControlGroup(): {
+  ControlRoot: React.ComponentClass<{
+    children: React.ReactNode;
+  }>;
+  Control: React.ComponentClass<{
+    tabIndex: number;
+    children: (
+      props: {
+        ref: React.RefObject<TextInput>;
+        onSubmitEditing: TextInputProps["onSubmitEditing"];
+        blurOnSubmit: false;
+      }
+    ) => React.ReactNode;
+  }>;
+};
+
+export type AndroidKeyboardDidHideEvent = null | undefined;
+export interface AndroidKeyboardDidShowEvent {
+  endCoordinates: {
+    screenX: number;
+    screenY: number;
+    width: number;
+    height: number;
   };
-
-  export type AndroidKeyboardDidHideEvent = null | undefined;
-  export interface AndroidKeyboardDidShowEvent {
-    endCoordinates: {
-      screenX: number;
-      screenY: number;
-      width: number;
-      height: number;
-    };
-  }
-  export interface IOSKeyboardEvent {
-    startCoordinates: {
-      screenX: number;
-      screenY: number;
-      width: number;
-      height: number;
-    };
-    endCoordinates: {
-      screenX: number;
-      screenY: number;
-      width: number;
-      height: number;
-    };
-    duration: number;
-    easing: "easeIn" | "easeInEaseOut" | "easeOut" | "linear" | "keyboard";
-  }
-  export type KeyboardEvent =
-    | AndroidKeyboardDidHideEvent
-    | AndroidKeyboardDidShowEvent
-    | IOSKeyboardEvent;
-
-  export interface KeyboardAvoidingViewProps {
-    behavior?: "margin";
-    androidSoftInputMode?: "adjustResize";
-  }
-  export class KeyboardAvoidingView extends React.Component<
-    KeyboardAvoidingViewProps
-  > {}
 }
+export interface IOSKeyboardEvent {
+  startCoordinates: {
+    screenX: number;
+    screenY: number;
+    width: number;
+    height: number;
+  };
+  endCoordinates: {
+    screenX: number;
+    screenY: number;
+    width: number;
+    height: number;
+  };
+  duration: number;
+  easing: "easeIn" | "easeInEaseOut" | "easeOut" | "linear" | "keyboard";
+}
+export type KeyboardEvent =
+  | AndroidKeyboardDidHideEvent
+  | AndroidKeyboardDidShowEvent
+  | IOSKeyboardEvent;
+
+export interface KeyboardAvoidingViewProps {
+  behavior?: "margin";
+  androidSoftInputMode?: "adjustResize";
+}
+export class KeyboardAvoidingView extends React.Component<
+  KeyboardAvoidingViewProps
+> {}
