16917de71aa5b4cf737a363333e62a62a0b455ad
Louis Chan <louischan@oursky.com>
2018-12-05 14:53:17 +0800

    Showcase we can import react-native-oursky

--- packages/react-native-oursky-example/App.js
+++ packages/react-native-oursky-example/App.js
@@ -1,50 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
+// @flow
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
-import { KeyboardAvoidingView } from "@oursky/react-native-oursky";
+import { StyleSheet, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  SignupWithMobile,
+} from "@oursky/react-native-oursky";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu",
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5FCFF",
+    justifyContent: "center",
+  },
 });
 
+// TODO: Make a full example.
+// Preferably a gallery of different components in their dedicated screens.
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <KeyboardAvoidingView style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <SignupWithMobile
+          description="Sign up with Mobile"
+          title="Sign up Now"
+          countryPickerProps={{
+            placeholder: "Calling Code",
+          }}
+          mobileNumberProps={{
+            placeholder: "National Number",
+          }}
+          submitButtonText="Submit"
+          skipButtonText="Skip"
+        />
       </KeyboardAvoidingView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10,
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5,
-  },
-});
