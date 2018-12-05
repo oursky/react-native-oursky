a55de41c1388c508117ed9da49e2ebea34dd15e9
kwguo <shana0440@gmail.com>
2018-12-05 16:19:30 +0800

    Add Verify OTP component example

--- packages/react-native-oursky-example/src/App.js
+++ packages/react-native-oursky-example/src/App.js
@@ -1,11 +1,15 @@
 // @flow
 import { createAppContainer, createDrawerNavigator } from "react-navigation";
 import SignupWithMobileScreen from "./screens/SignupWithMobileScreen";
+import VerifyOTPScreen from "./screens/VerifyOTPScreen";
 
 const AppNavigator = createDrawerNavigator(
   {
     SignupWithMobile: {
       screen: SignupWithMobileScreen,
+    },
+    VerifyOTP: {
+      screen: VerifyOTPScreen,
     },
   },
   {
