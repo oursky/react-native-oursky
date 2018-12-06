5757a96ef92eb2f9794d3338faeef94a67438423
kwguo <shana0440@gmail.com>
2018-12-06 18:13:59 +0800

    Format .java

--- packages/react-native-oursky-example/android/app/src/main/java/com/reactnativeourskyexample/MainApplication.java
+++ packages/react-native-oursky-example/android/app/src/main/java/com/reactnativeourskyexample/MainApplication.java
@@ -3,11 +3,11 @@ package com.reactnativeourskyexample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGestureHandlerPackage()
+          new RNGestureHandlerPackage()
       );
     }
 
