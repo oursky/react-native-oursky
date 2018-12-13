3ee8eaa02b06ae7388bd84e291562d4151e191ad
Wallace Hui <wallace@oursky.com>
2018-12-13 19:34:53 +0800

    Introduce NetworkFailureToast with simple template ui #7

---
+++ packages/react-native-oursky/src/NetworkFailureToast.js
@@ -0,0 +1,59 @@
+// @flow
+import * as React from "react";
+import { SafeAreaView, StyleSheet, View, Image } from "react-native";
+import Text from "./Text";
+
+const styles = StyleSheet.create({
+  safeArea: {
+    bottom: 0,
+    left: 0,
+    right: 0,
+    position: "absolute",
+    alignItems: "center",
+  },
+  toastContainer: {
+    backgroundColor: "white",
+    width: 349,
+    height: 65,
+    bottom: 69,
+    borderColor: "#CACACA",
+    borderWidth: 1,
+    borderRadius: 6,
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  icon: {
+    marginHorizontal: 14,
+  },
+  text: {
+    fontSize: 14,
+    color: "#333333",
+  },
+  textContainer: {
+    flex: 1,
+    paddingRight: 14,
+  },
+});
+
+export default class NetworkFailureToast extends React.PureComponent {
+  render() {
+    return (
+      <SafeAreaView style={styles.safeArea} pointerEvents={"box-none"}>
+        <View style={styles.toastContainer}>
+          <Image
+            style={styles.icon}
+            source={require("./images/toast_offline_icon.png")}
+            resizeMode="center"
+          />
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>
+              Cannot reach internet. Please check your device internet
+              connections.
+            </Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
