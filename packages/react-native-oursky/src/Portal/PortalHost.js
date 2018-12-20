9944da676c7d7805e5a853f0966e0f65a966bc7c
YinYin Chiu <cychiuae@connect.ust.hk>
2018-12-20 17:50:48 +0800

    Introduce PortalHost for mounting component

---
+++ packages/react-native-oursky/src/Portal/PortalHost.js
@@ -0,0 +1,81 @@
+// @flow
+import * as React from "react";
+
+type PortalKey = number;
+type PortalMethods = {
+  mount: (children: React.Node) => PortalKey,
+  update: (key: PortalKey, children: React.Node) => void,
+  unmount: (key: PortalKey) => void,
+};
+
+type RenderedPortal = {
+  key: PortalKey,
+  children: React.Node,
+};
+
+type Props = {
+  children: React.Node,
+};
+
+type State = {
+  portalMethods: PortalMethods,
+  portals: RenderedPortal[],
+};
+
+export const PortalContext = React.createContext<PortalMethods>((null: any));
+
+class PortalHost extends React.Component<Props, State> {
+  nextPortalKey = 0;
+  state = {
+    portals: [],
+    portalMethods: {
+      mount: this.mount,
+      update: this.update,
+      unmount: this.unmount,
+    },
+  };
+
+  mount = (children: React.Node): PortalKey => {
+    const portalKey = this.nextPortalKey++;
+    this.setState(prevState => ({
+      portals: [
+        ...prevState.portals,
+        {
+          key: portalKey,
+          children,
+        },
+      ],
+    }));
+    return portalKey;
+  };
+
+  update = (key: PortalKey, children: React.Node) => {
+    this.setState(prevState => ({
+      portals: prevState.portals.map(portal => {
+        if (portal.key === key) {
+          return {
+            key,
+            children,
+          };
+        }
+        return portal;
+      }),
+    }));
+  };
+
+  unmount = (key: PortalKey) => {
+    this.setState(prevState => ({
+      portals: prevState.portals.filter(p => p.key !== key),
+    }));
+  };
+
+  render() {
+    return (
+      <PortalContext.Provider value={this.state.portalMethods}>
+        {this.props.children}
+      </PortalContext.Provider>
+    );
+  }
+}
+
+export default PortalHost;
